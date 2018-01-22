0661f83c416b47a9af6ea3f408a20a34f446dd75
Matteo Iervasi <matteoiervasi@gmail.com>
2018-01-22 11:01:39 +0100

    Corretti errori negativi

--- Analyzer.py
+++ Analyzer.py
@@ -20,14 +20,14 @@ from enum import Enum
 
 
 class Error(Enum):
-    MISSING_PROJECT_ARCHIVE = -1
-    MISSING_CORRECT_INPUT = -2
-    MISSING_CORRECT_OUTPUT = -3
-    CORRECTNESS_CALCULATION_ERROR = -4
-    SIMULATION_ERROR = -5
-    MALFORMED_ARCHIVE = -6
-    UNIDENTIFIED_PROJECT_TYPE = -7
-    NOT_A_TARBALL = -8
+    MISSING_PROJECT_ARCHIVE = 1
+    MISSING_CORRECT_INPUT = 2
+    MISSING_CORRECT_OUTPUT = 3
+    CORRECTNESS_CALCULATION_ERROR = 4
+    SIMULATION_ERROR = 5
+    MALFORMED_ARCHIVE = 6
+    UNIDENTIFIED_PROJECT_TYPE = 7
+    NOT_A_TARBALL = 8
 
 
 # Check and manage arguments
@@ -50,13 +50,13 @@ args = parser.parse_args()
 
 if not os.path.exists(args.file) or not os.path.isfile(args.file):
     print("An error occurred. Please specify the path of the archive that contains the projects.")
-    sys.exit(Error.MISSING_PROJECT_ARCHIVE)
+    sys.exit(Error.MISSING_PROJECT_ARCHIVE.value)
 if not os.path.exists(args.inp) or not os.path.isfile(args.inp):
     print("An error occurred. Please specify the path of the simulation input.")
-    sys.exit(Error.MISSING_CORRECT_INPUT)
+    sys.exit(Error.MISSING_CORRECT_INPUT.value)
 if not os.path.exists(args.out) or not os.path.isfile(args.out):
     print("An error occurred. Please specify the path of the simulation output.")
-    sys.exit(Error.MISSING_CORRECT_OUTPUT)
+    sys.exit(Error.MISSING_CORRECT_OUTPUT.value)
 
 simulation_input = str(os.path.abspath(args.inp))
 correct_outputs = str(os.path.abspath(args.out))
@@ -73,20 +73,20 @@ if tar_archive.endswith(".tar.gz") or tar_archive.endswith(".tar.xz"):
                     sys.exit(0)
                 else:
                     print("Error during correctness calculation")
-                    sys.exit(Error.CORRECTNESS_CALCULATION_ERROR)
+                    sys.exit(Error.CORRECTNESS_CALCULATION_ERROR.value)
             else:
                 print("Something went wrong during the simulation")
-                sys.exit(Error.SIMULATION_ERROR)
+                sys.exit(Error.SIMULATION_ERROR.value)
         else:
             print("The archive does not respect the correct structure")
-            sys.exit(Error.MALFORMED_ARCHIVE)
+            sys.exit(Error.MALFORMED_ARCHIVE.value)
 
     elif "asm" in tar_archive:  # TODO: da fare gestione assembly
         asm_tar_directory = AnalyzerSis.extract_archive(tar_archive)
 
     else:
         print("Error! Project not identified!")
-        sys.exit(Error.UNIDENTIFIED_PROJECT_TYPE)
+        sys.exit(Error.UNIDENTIFIED_PROJECT_TYPE.value)
 else:
     print("The given archive is not a TAR archive")
-    sys.exit(Error.NOT_A_TARBALL)
+    sys.exit(Error.NOT_A_TARBALL.value)
